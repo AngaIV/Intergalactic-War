910ae6fdc94dcb9caa13b40cd8d15d3b48d9a277
Anga <anga.peter1@icloud.com>
2023-09-06 22:52:28 +0200

    Latest developments to the game

---
+++ Panel.java
@@ -0,0 +1,54 @@
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.*;
+import java.lang.Runnable;
+
+public class Panel extends JPanel implements Runnable{
+    
+    public static int w = 500;
+    public static int h = 500;
+    private Thread thread;
+    private boolean isRunning;
+    private BufferedImage image; //canvas
+    private Graphics2D g;
+    
+    public Panel() { //Construct
+        super();
+        setPreferredSize(new Dimension(w, h));
+        setFocusable(true);
+        requestFocus();
+    }
+    //Fuctions
+    public void addNotify() {
+        super.addNotify();
+        if(thread==null){
+            thread = new Thread((Runnable) this);
+            thread.start();
+        }   
+    }
+    public void run(){
+        isRunning = true;
+        image = new BufferedImage (w, h, BufferedImage.TYPE_INT_RGB);
+        g = (Graphics2D) image.getGraphics();
+        
+        while (isRunning){ //loop
+            Update();
+            Render();
+            Draw();
+        }
+    }
+    private void Update() { //logic of the game
+    }
+    
+    private void Render(){
+       g.setColor(Color.black);
+       g.fillRect(0, 0, w, h);
+       g.setColor(Color.white);
+       g.drawString("LESS GOOO", 100, 100);
+    }
+    private void Draw(){
+        Graphics g1 = this.getGraphics();
+        g1.drawImage(image, 0, 0, null);
+        g1.dispose();
+    }
+}
