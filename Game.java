e0078056eb5396f4b55e472e18f2046d1a4a6973
AngaIV <125663394+AngaIV@users.noreply.github.com>
2023-09-06 23:48:24 +0200

    Add files via upload
    Updated Development

---
+++ Game.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import java.lang.Runnable;
+import javax.swing.ImageIcon;
+
+public class Game {
+    public static void main(String[] args){
+        ImageIcon logo = new ImageIcon(".//res//Alien.png");
+        JFrame window = new JFrame("IntergalacticWars");
+        window.setIconImage(logo.getImage());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setContentPane(new Panel());
+        window.pack();
+        window.setVisible(true);
+    }
+}
---
+++ Player.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+
+public class Player {
+    
+    //Fields
+    private int x_int;
+    private int y_int;
+    private int radius;
+    
+    private int dirX;
+    private int dirY;
+    private int speedofplayer;
+    
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
+    
+    private int lives;
+    
+    //LETS CONSTRUUCTTTT!!!!
+    public Player() {
+    x_int = Panel.WIDTH /2;
+    y_int = Panel.HEIGHT /2;
+    radius = 5;
+    
+    dirX =0;
+    dirY = 0;
+    speedofplayer = 5;
+    lives = 5;
+    }
+    
+    public void update() {
+        if(left) {
+            dirX = -speedofplayer;
+        }
+        if (right) {
+            dirX = speedofplayer;
+        }
+        if (up){
+            dirY = -speedofplayer;
+        }
+        if(down) {
+            dirY = speedofplayer;
+        }
+        x_int += dirX;
+        y_int += dirY;
+        if(x_int < radius) x_int = radius;
+        if(y_int < radius) y_int = radius;
+        if(x_int > Panel.WIDTH - radius) x_int = Panel.WIDTH - radius;
+        if(x_int > Panel.HEIGHT - radius) x_int = Panel.HEIGHT - radius;
+    }
+    public void draw(Graphics2D g) {
+        
+    }
+}
