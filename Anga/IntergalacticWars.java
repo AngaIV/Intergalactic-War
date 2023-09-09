9a4f5885761e19a931d453156f5872ad1586cec1
AngaIV <125663394+AngaIV@users.noreply.github.com>
2023-09-09 19:45:35 +0200

    Add files via upload

---
+++ Anga/IntergalacticWars.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gigganiggas.intergalacticwars;
+
+/**
+ *
+ * @author sixol
+ */
+public class IntergalacticWars {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
